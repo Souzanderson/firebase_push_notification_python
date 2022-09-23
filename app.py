97712fb674bec1c5f5e6fb91fce0cde36e8946cd
Anderson <andersonsouza.utfpr@gmail.com>
2022-09-23 10:37:12 -0300

    send message for push notifications

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ from beans.firebasemessage import PushNotification, MessagePushNotification
 # @param topic => tópico para envio e recepção de mensagens, existem 3 tópicos configurados: lines, system, user_<idUser>
 # @param title => título da notificação no mobile
 # @param body => corpo da mensagem da notificação recebida no mobile
-agent = MessagePushNotification(topic="bundy_cwb_lines", title= "Mensagem direcionada a todos", body= "Mensagem direcionada a todos!")
+agent = MessagePushNotification(topic="system", title= "Sistema!", body= "Existem n novas ocorrências aguardando solução...")
 
 # @param message_agent => objeto contendo a mensagem para envio
 # @param credentials_location => arquivo de autenticação com o servidor de push notification (padrão firebase_key.json)
 # @def send => envia a mensagem usando o arquivo de configuração 
-PushNotification(message_agent = agent).send()
+PushNotification(message_agent = agent, credentials_location="test_firebase_key.json").send()
